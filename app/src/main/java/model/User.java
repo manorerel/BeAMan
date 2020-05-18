725ef2de89ed22d2b57f4b445a6e2e4cd422069c
manor <manor.erel20@gmail.com>
2020-05-19 00:42:32 +0300

    update user entity

--- app/src/main/java/model/User.java
+++ app/src/main/java/model/User.java
@@ -6,11 +6,19 @@ public class User {
     String _id;
     String _userName;
     String _password;
+    String _mail;
+    String _firstName;
+    String _lastName;
+    Boolean _isTherapist;
 
-    public User(String userName, String password){
+    public User(String userName, String password, String mail, String firstName, String lastName, Boolean isTherapist){
         _id = UUID.randomUUID().toString();
         _userName = userName;
         _password = password;
+        _firstName = firstName;
+        _lastName = lastName;
+        _mail = mail;
+        _isTherapist = isTherapist;
     }
 
     public String get_id(){return _id;}
@@ -21,4 +29,9 @@ public class User {
 
     public String get_password(){return _password;}
     public void set_password0(String password){_password = password;}
+
+    public String get_mail(){return _mail;}
+    public String get_firstName(){return _firstName;}
+    public String get_lastName(){return _lastName;}
+    public Boolean get_isTherapist(){return _isTherapist;}
 }
