d75b5bb179d15a1492b28d0871458d361851571b
manor <manor.erel20@gmail.com>
2020-05-18 12:52:01 +0300

    add the main necessary activities

--- app/src/main/java/com/michalsela/beaman/MainActivity.java
+++ app/src/main/java/com/michalsela/beaman/MainActivity.java
@@ -2,7 +2,10 @@ package com.michalsela.beaman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button guidButton = findViewById(R.id.guid_button_id);
+        guidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGuidActivity();
+            }
+        });
+
+        Button treatButton = findViewById(R.id.treat_button_id);
+        treatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTreatActivity();
+            }
+        });
+    }
+
+    private void openGuidActivity(){
+        Intent guidIntent = new Intent(this, GuidnaceActivity.class);
+        startActivity(guidIntent);
+    }
+
+    private void openTreatActivity(){
+        Intent treatIntent = new Intent(this, MainTreatmentActivity.class);
+        startActivity(treatIntent);
     }
 }
