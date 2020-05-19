0789c2564024717862ac0761088d1a547c8be754
manor <manor.erel20@gmail.com>
2020-05-19 11:36:42 +0300

    add group video chat activity

---
+++ app/src/main/java/com/michalsela/beaman/GroupVideoChatActivity.java
@@ -0,0 +1,14 @@
+package com.michalsela.beaman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupVideoChatActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_video_chat);
+    }
+}
