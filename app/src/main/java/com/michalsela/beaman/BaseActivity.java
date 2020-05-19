728d2e4e6b1e3bf0f591ef4937cddc8067dd1856
manor <manor.erel20@gmail.com>
2020-05-19 20:28:55 +0300

    action bar

--- app/src/main/java/com/michalsela/beaman/BaseActivity.java
+++ app/src/main/java/com/michalsela/beaman/BaseActivity.java
@@ -3,9 +3,14 @@ package com.michalsela.beaman;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -21,4 +26,39 @@ public class BaseActivity extends AppCompatActivity {
         color = new ColorDrawable(getResources().getColor(R.color.green));
         supportActionBar.setBackgroundDrawable(color);
     }
+
+    @SuppressLint({"RestrictedApi", "ResourceType"})
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.layout.mainmenu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.user_self_place:{
+                return true;
+            }
+            case R.id.personal_treat: {
+                Intent treatIntent = new Intent(this, TherapistsListActivity.class);
+                treatIntent.putExtra("kind","personal");
+                startActivity(treatIntent);
+                return true;
+            }
+            case R.id.group_treat: {
+                Intent groupVideoChatIntent = new Intent(this, TherapistsListActivity.class);
+                groupVideoChatIntent.putExtra("kind","group");
+                startActivity(groupVideoChatIntent);
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
